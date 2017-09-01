c35dd438b1fa134008be7311573a4d2b73e52023
Vincent Tsang <vstsang@gmail.com>
2017-09-02 06:31:11 +0800

    Retrieve mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,6 +1,7 @@
 require 'httparty'
 require 'json'
 
+
 class Kele
   include HTTParty
   @@base_uri = 'https://www.bloc.io/api/v1'
@@ -13,7 +14,11 @@ class Kele
 
   def get_me
     response = self.class.get("#{@@base_uri}/users/me", headers: { "authorization" => @auth_token })
-    raise response["message"] if response.code != 200
-    user_json = JSON.parse(response.body)
+    @user_data = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("#{@@base_uri}/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
   end
 end
