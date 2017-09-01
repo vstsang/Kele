824e4d2a804bfb251cc39e90e6e015b6c55fdc4e
Vincent Tsang <vstsang@gmail.com>
2017-09-01 20:36:07 +0800

    Retrieve JSON user data

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,12 +1,19 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+  @@base_uri = 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    @base_uri = 'https://www.bloc.io/api/v1'
-    response = self.class.post("#{@base_uri}/sessions", body: {"email": email, "password": password})
+    response = self.class.post("#{@@base_uri}/sessions", body: {"email": email, "password": password})
     raise response["message"] if response.code != 200
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get("#{@@base_uri}/users/me", headers: { "authorization" => @auth_token })
+    raise response["message"] if response.code != 200
+    user_json = JSON.parse(response.body)
   end
 end
